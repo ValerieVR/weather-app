6661dad528ce786eb06a1ff28787afadfd191be1
Valerie <akpotevalerie@gmail.com>
2020-12-31 00:44:24 +0100

    Add date arrays

--- script.js
+++ script.js
@@ -6,6 +6,10 @@
 
     let searchCity2 = document.getElementById("search-input-2");
     let searchCity2Bttn = document.getElementById("search-button-2");
+
+    let longDays = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+    let shortDays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+    let shortMonths = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
     
 
 
@@ -32,10 +36,14 @@
         try {
             let response = await fetch("https://api.weatherbit.io/v2.0/current?city="+city2+"&key=85f3c391448f45a4a7c0387acc9baedf");
             let weatherData = await response.json();
-            console.log(weatherData);
+            displayWeather_City2(weatherData);
         } catch (error) {
             alert("Can't display weather. Something went wrong.")
         }
+    }
+
+    let displayWeather_City2 = (data) => {
+
     }
 
     
