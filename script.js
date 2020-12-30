811681d3adfc86a49b4b4747d6a46cacf0ad4c51
Valerie <akpotevalerie@gmail.com>
2020-12-30 21:54:39 +0100

    Add function to fetch weather data of second city

--- script.js
+++ script.js
@@ -4,6 +4,12 @@
     let extraCityBttn = document.getElementById("extra-city-button");
     let extraCity = document.getElementById("outer-container-3");
 
+    let searchCity2 = document.getElementById("search-input-2");
+    let searchCity2Bttn = document.getElementById("search-button-2");
+    
+
+
+
     let changePlusMinus = () => {
         if (extraCityBttn.innerHTML === "+") {
             extraCityBttn.classList.add("minus-js");
@@ -22,10 +28,29 @@
         }
     }
 
+    let getWeatherData_City2 = async (city2) => {
+        try {
+            let response = await fetch("https://api.weatherbit.io/v2.0/current?city="+city2+"&key=85f3c391448f45a4a7c0387acc9baedf");
+            let weatherData = await response.json();
+            console.log(weatherData);
+        } catch (error) {
+            alert("Can't display weather, try again!")
+        }
+    }
+
+    
+
     extraCityBttn.addEventListener("click", () => {
         changePlusMinus();
         add_remove_newCity();
     });
+
+    searchCity2Bttn.addEventListener("click", (e) => {
+        e.preventDefault();
+        getWeatherData_City2(searchCity2.value);
+    });
+
+
 
 
 
