2cdd0cb419eb47fe51646d49b5dcc212c41d7f0e
Valerie <akpotevalerie@gmail.com>
2020-12-31 17:22:01 +0100

    Add button first city

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 (() => {
     "use strict";
 
+
     let extraCityBttn = document.getElementById("extra-city-button");
     let extraCity = document.getElementById("outer-container-3");
 
     let searchCity2 = document.getElementById("search-input-2");
     let searchCity2Bttn = document.getElementById("search-button-2");
 
+    let searchCity1 = document.getElementById("search-input-1");
+    let searchCity1Bttn = document.getElementById("search-button-1");
+
+
     let longDays = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
     let shortDays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
     let shortMonths = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
     
-
-
 
     let changePlusMinus = () => {
         if (extraCityBttn.innerHTML === "+") {
@@ -32,19 +35,11 @@
         }
     }
 
-    let getWeatherData_City2 = async (city2) => {
-        try {
-            let response = await fetch("https://api.weatherbit.io/v2.0/current?city="+city2+"&key=85f3c391448f45a4a7c0387acc9baedf");
-            let weatherData = await response.json();
-            displayWeather_City2(weatherData);
-        } catch (error) {
-            alert("Can't display weather. Something went wrong.")
-        }
-    }
+    
 
-    let displayWeather_City2 = (data) => {
+ 
 
-    }
+   
 
     
 
@@ -53,9 +48,12 @@
         add_remove_newCity();
     });
 
+    searchCity1Bttn.addEventListener("click", (e) => {
+        e.preventDefault();
+    })
+
     searchCity2Bttn.addEventListener("click", (e) => {
         e.preventDefault();
-        getWeatherData_City2(searchCity2.value);
     });
 
 
