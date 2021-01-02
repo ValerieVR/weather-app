d96e9ebf882a8f1ee2ff7c5308f2c53e6174d6de
Valerie <akpotevalerie@gmail.com>
2021-01-02 22:28:00 +0100

    Add function to display weather current location

--- script.js
+++ script.js
@@ -35,6 +35,91 @@
         }
     }
 
+    let getCurrentLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(getWeatherCurrentLocation);
+        } else {
+            let msg = geolocationPositionErrorInstance.message;
+            console.error(msg);
+        }
+    }
+
+    let getWeatherCurrentLocation = async (position) => {
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
+        try {
+            let response = await fetch("https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&units=metric&appid=2ef359fbac037127dbef3b700cdb7730");
+            let weatherData = await response.json();
+
+            let i = 1;
+            for (let j = 0; j < weatherData.list.length; j += 8) {
+
+                let day = document.getElementById("day-" + i);
+                let date_str_day = new Date(weatherData.list[j].dt_txt);
+                if (j === 0) {
+                    let day_long = longDays[date_str_day.getDay()];
+                    day.innerHTML = day_long;
+                } else {
+                    let day_short = shortDays[date_str_day.getDay()];
+                    day.innerHTML = day_short;
+                }
+
+                let temp = document.getElementById("temp-" + i);
+                let temperature = Math.round(weatherData.list[j].main.temp);
+                temp.innerHTML = temperature;
+
+                let image = document.getElementById("weather-icon-" + i);
+                let code = weatherData.list[j].weather[0].id;
+
+                if (code >= 200 && code < 300) {
+                    image.src = "./images/thunderstorm.svg";
+                } else if (code >= 300 && code < 500) {
+                    image.src = "./images/drizzle.svg"
+                } else if (code >= 500 && code < 600) {
+                    image.src = "./images/rain.svg"
+                } else if (code >= 600 && code <= 700) {
+                    image.src = "./images/snow.svg"
+                } else if (code > 700 && code < 800) {
+                    image.src = "./images/foggy.svg"
+                } else if (code == 800) {
+                    let time = date_str_day.getHours();
+                    if (time > 5) {
+                        image.src = "./images/sun.svg";
+                    } else {
+                        image.src = "./images/moon.svg";
+                    }
+                } else {
+                    image.src = "./images/clouds.svg";
+                }
+        
+                i++;
+            }
+
+            let date_str_month = new Date(weatherData.list[0].dt_txt);
+            
+            let month = document.getElementById("month-short-1");
+            let month_short = shortMonths[date_str_month.getMonth()];
+            month.innerHTML = month_short;
+
+            let dayDate = document.getElementById("date-day-1");
+            let dayNum = date_str_month.getDate();
+            dayDate.innerHTML = dayNum;
+
+            let clim = document.getElementById("climate-1");
+            let climate = weatherData.list[0].weather[0].description;
+            clim.innerHTML = climate;
+
+            location
+            let currentLocation = weatherData.city.name;
+            let city = document.getElementById("city");
+            city.innerHTML = currentLocation;
+
+        } catch (error) {
+            alert("Can't display weather current location.")
+            console.log(error);
+        }
+    }
+
     let getWeatherCity1 = async (city1) => {
         try {
             let response = await fetch("https://api.openweathermap.org/data/2.5/forecast?q="+city1+"&units=metric&appid=2ef359fbac037127dbef3b700cdb7730");
@@ -170,10 +255,14 @@
     }
 
 
-
     let displayCity = () => {
         city1.innerHTML = searchCity1.value;
     }
+
+
+    window.addEventListener("load", () => {
+        getCurrentLocation();
+    })
 
     extraCityBttn.addEventListener("click", () => {
         changePlusMinus();
